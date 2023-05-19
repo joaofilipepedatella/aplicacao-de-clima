05d9e39fd0a9fbc0c15e576ce98703980e834b94
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2023-05-19 18:18:24 -0300

    Finalizacao

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,111 @@
-// 8f6c85f7fa772ba65bb3580d5d6bf4ab
+const apiKey = "8f6c85f7fa772ba65bb3580d5d6bf4ab"
+const apiCountryURL = "https://flagsapi.com/"
+const apiUnsplash = "https://source.unsplash.com/1600x900/?"
+
+const cityInput = document.querySelector("#city-input")
+const searchBtn = document.querySelector("#search")
+
+const cityElement = document.querySelector("#city")
+const TempElement = document.querySelector("#temperature span")
+const descElement = document.querySelector("#description")
+const weatherIconElement = document.querySelector("#weather-icon")
+const countryElement = document.querySelector("#country")
+const humidityElement = document.querySelector("#humidity span")
+const windElement = document.querySelector("#wind span")
+
+const weatherContainer = document.querySelector("#weather-data")
+
+const errorMessageContainer = document.querySelector("#error-message")
+const loader = document.querySelector("#loader")
+
+const suggestionContainer = document.querySelector("#suggestions")
+const suggestionButtons = document.querySelectorAll("#suggestions button")
+
+// Loader
+
+const toggleLoader = () => {
+    loader.classList.toggle("hide")
+}
+
+const getWeatherData = async (city) => {
+    toggleLoader()
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    toggleLoader()
+
+    return data
+}
+
+// Tratamento de erro 
+
+const showErrorMessage = () => {
+    errorMessageContainer.classList.remove("hide")
+}
+
+const hideInformation = () => {
+    errorMessageContainer.classList.add("hide")
+    weatherContainer.classList.add("hide")
+
+    suggestionContainer.classList.add("hide")
+}
+
+const showWeatherData = async (city) => {
+    hideInformation()
+
+    const data = await getWeatherData(city)
+
+    if(data.cod === "404"){
+        showErrorMessage()
+
+        return
+    }
+
+
+    cityElement.innerText = data.name
+    TempElement.innerText = parseInt(data.main.temp)
+    descElement.innerText = data.weather[0].description
+    weatherIconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    )
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country + '/flat/64.png')
+    humidityElement.innerText = `${data.main.humidity}%`
+    windElement.innerText = `${data.wind.speed}km/h`
+
+    //Mudar imagem de background 
+
+    document.body.style.backgroundImage = `url("${apiUnsplash + city}")`
+
+    weatherContainer.classList.remove("hide")
+}
+
+searchBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+
+    const city = cityInput.value
+
+    showWeatherData(city)
+    cityInput.value = ''
+})
+
+cityInput.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+        const city = e.target.value
+
+        showWeatherData(city)
+    }
+})
+
+// Sugestoes
+
+suggestionButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const city = btn.getAttribute("id")
+
+        showWeatherData(city)
+    })
+})
